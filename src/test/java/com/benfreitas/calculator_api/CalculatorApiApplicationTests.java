5438d798df1ff955aa32863f52a4b0ca04509882
Benjamim Freitas <Ben.FreitasG@gmail.com>
2024-06-21 21:25:10 -0300

    Teste pipeline com test unitario falhando

--- src/test/java/com/benfreitas/calculator_api/CalculatorApiApplicationTests.java
+++ src/test/java/com/benfreitas/calculator_api/CalculatorApiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.benfreitas.calculator_api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.benfreitas.calculator_api.services.CalculatorService;
 
 @SpringBootTest
 class CalculatorApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void addition() {
+		assertEquals(new BigDecimal(10), CalculatorService.add(new BigDecimal(7), new BigDecimal(3)));
 	}
 
+	@Test
+	void additionWithNegative() {
+		assertEquals(new BigDecimal(4), CalculatorService.add(new BigDecimal(7), new BigDecimal(-2)));
+	}
 }
