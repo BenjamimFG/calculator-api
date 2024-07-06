b6fe8c7c86afc702734a7d6580cf0abeb47c8af8
Benjamim Freitas <Ben.FreitasG@gmail.com>
2024-07-05 21:52:38 -0300

    Adiciona prometheus e grafana

--- src/main/java/com/benfreitas/calculator_api/GlobalExceptionHandler.java
+++ src/main/java/com/benfreitas/calculator_api/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.benfreitas.calculator_api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.benfreitas.calculator_api.exceptions.DivisionByZeroException;
 import com.benfreitas.calculator_api.models.ErrorResponse;
@@ -12,11 +14,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @ControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+  
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DivisionByZeroException.class)
     @ResponseBody
     public ErrorResponse handleDivisionByZero(final DivisionByZeroException ex) {
         return new ErrorResponse(ex.getMessage());
     }
+
+    //@ExceptionHandler(NoHandlerFoundException.class)
+    //@ResponseStatus(HttpStatus.NOT_FOUND)
+    //@ResponseBody
+    //public ErrorResponse handleNoHandlerFound(NoHandlerFoundException e) {
+    //    return new ErrorResponse("Not found");
+    //}
 }
--- src/main/java/com/benfreitas/calculator_api/controllers/CalculatorController.java
+++ src/main/java/com/benfreitas/calculator_api/controllers/CalculatorController.java
@@ -10,16 +10,49 @@ import com.benfreitas.calculator_api.exceptions.DivisionByZeroException;
 import com.benfreitas.calculator_api.models.CalculatorResult;
 import com.benfreitas.calculator_api.services.CalculatorService;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 public class CalculatorController {
+  
+    private Counter addCounter;
+    private Counter subtractCounter;
+    private Counter multiplyCounter;
+    private Counter divideCounter;
+
+    public CalculatorController(MeterRegistry registry) {
+        this.addCounter = Counter.builder("add_requests_total").
+                tag("version", "v1").
+                description("Contador de requisições 'add'.").
+                register(registry);
+
+        this.subtractCounter = Counter.builder("sub_requests_total").
+                tag("version", "v1").
+                description("Contador de requisições 'subtract'.").
+                register(registry);
+
+        this.multiplyCounter = Counter.builder("mul_requests_total").
+                tag("version", "v1").
+                description("Contador de requisições 'multiply'.").
+                register(registry);
+
+        this.divideCounter = Counter.builder("div_requests_total").
+                tag("version", "v1").
+                description("Contador de requisições 'divide'.").
+                register(registry);
+
+    }
     
     @GetMapping("/add/{a}/{b}")
     public CalculatorResult add(@PathVariable BigDecimal a, @PathVariable BigDecimal b) {
+        this.addCounter.increment();
         return new CalculatorResult(CalculatorService.add(a, b));
     }
 
     @GetMapping("/divide/{a}/{b}")
     public CalculatorResult divide(@PathVariable BigDecimal a, @PathVariable BigDecimal b) {
+        this.divideCounter.increment();
         try {
             BigDecimal result = CalculatorService.divide(a, b);
 
@@ -31,13 +64,13 @@ public class CalculatorController {
     
     @GetMapping("/subtract/{a}/{b}")
     public CalculatorResult subtract(@PathVariable BigDecimal a, @PathVariable BigDecimal b) {
-    	
+    	this.subtractCounter.increment();
     	return new CalculatorResult(CalculatorService.subtract(a, b));
     }
     
     @GetMapping("/multiply/{a}/{b}")
     public CalculatorResult multiply(@PathVariable BigDecimal a, @PathVariable BigDecimal b) {
-    	
+    	this.multiplyCounter.increment();
     	return new CalculatorResult(CalculatorService.multiply(a, b));
     }
     
