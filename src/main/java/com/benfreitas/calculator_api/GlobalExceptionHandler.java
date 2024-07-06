0d57e1ba6f1d96fedb45bbef1809dc2a3966e529
Benjamim Freitas <Ben.FreitasG@gmail.com>
2024-07-06 06:27:08 -0300

    Adiciona provisioning ao grafana

--- src/main/java/com/benfreitas/calculator_api/GlobalExceptionHandler.java
+++ src/main/java/com/benfreitas/calculator_api/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.benfreitas.calculator_api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.benfreitas.calculator_api.exceptions.DivisionByZeroException;
